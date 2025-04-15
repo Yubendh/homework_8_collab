1d4a7b7c3a7cd7ad76e5d50b022fcb0c5555a9f2
YouhourLim <2024218lim@aupp.edu.kh>
2025-04-15 16:44:42 +0700

    Add chunking functionality to exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,12 @@
 def chunking_by(numbers, chunck):
-    ...
+    chunks_list = []
+    
+    # Loop through the list, stepping by chunk_size
+    for i in range(0, len(numbers), chunck):
+        # Get a chunk from the list using slicing
+        chunks_list = numbers[i:i + chunck]
+        # Add the chunk to the list of chunks
+        chunks_list.append(chunks_list)
+    
+    # Return the final list of chunks
+    return chunks_list
